e55a839d826dca097a54ac81586fb09b2ad32708
Dylan Pee <peedylan94@gmail.com>
2025-09-08 16:04:23 +0930

    Ep 02: Replaced React CDN and installed react & react-dom as dependencies; Successfully ran npx parcel index.html

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 // const heading = React.createElement("h1", { id: "heading", abc: "xyz" }, "Hello World from React!");
 
 // const parent = React.createElement(
@@ -10,14 +14,14 @@
 //   )
 // )
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a h1"),
-    React.createElement("h2", {}, "I am a h2"),
+const parent = React.createElement("div", { id: "parent", key: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I am a h1"),
+    React.createElement("h2", { key: "h2-1" }, "I am a h2"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am also a h1"),
-    React.createElement("h2", {}, "I am also a h2"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I am also a h1"),
+    React.createElement("h2", { key: "h2-2" }, "I am also a h2"),
   ]),
 ]);
 
