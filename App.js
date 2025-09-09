9908f3599ebd213f2dced2a125b2539482d47212
Dylan Pee <peedylan94@gmail.com>
2025-09-09 11:13:40 +0930

    Ep 03: Example of React Element and React Functional Component

--- App.js
+++ App.js
@@ -1,2 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//* React Element
+
+const jsxHeading = (
+  <h1 id="heading" className="head" tabIndex="5">
+    Writting JSX inside of JS file
+  </h1>
+);
+
+//* React Functional Component
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading">
+      Namaste React Functional Component
+    </h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
