e50d7843c6390f47e4a9d00dbd7cafc0668868ec
Dylan Pee <peedylan94@gmail.com>
2025-09-09 11:17:58 +0930

    Ep 03: Example of writing JS code inside of JSX code

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//* React Component Composition
 
 const JsxHeading = () => (
   <h1 id="heading" className="head" tabIndex="5">
@@ -9,9 +8,13 @@ const JsxHeading = () => (
   </h1>
 );
 
+//* JS code inside of JSX
+
+const number = 100;
+
 const HeadingComponent = () => (
   <div id="container">
-    <JsxHeading />
+    {number}
     <h1 className="heading">
       Namaste React Functional Component
     </h1>
