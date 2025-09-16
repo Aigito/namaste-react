01cf18c055356ff0300f2b2fb5f4ad9e82b51431
Dylan Pee <peedylan94@gmail.com>
2025-09-16 18:08:51 +0930

    Ep 07: Build out error page with a bit more detail

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,6 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
 const PageNotFound = () => {
+  const err = useRouteError();
+  console.log(err);
+
   return (
-    <h1>The page you are looking for does not exist!</h1>
+    <div>
+      <h1>The page you are looking for does not exist!</h1>
+      <h2>Error message: {err.status} {err.statusText} {err.error.message}</h2>
+      <h3>Err.status: {err.status}</h3>
+      <h3>Err.statusText: {err.statusText}</h3>
+      <h3>Err.error.message: {err.error.message}</h3>
+    </div>
   )
 };
 
