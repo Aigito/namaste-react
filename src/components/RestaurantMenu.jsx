773f3c26b5ef5b70b52e2491589713ff43e71b55
Dylan Pee <peedylan94@gmail.com>
2025-10-14 11:53:31 +1030

    Ep 07: WIP trying to get Swiggy API to work

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const RestaurantMenu = () => {
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const url = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0035068&lng=77.5890953&restaurantId=425&catalog_qa=undefined&submitAction=ENTER";
+    const res = await fetch("https://corsproxy.io/?url=" + url);
+    console.log(res.ok, res.status);
+    const text = await res.text();
+    console.log(text)
+  };
+
   return (
     <div className="menu">
       <h1>Restaurant Name</h1>
