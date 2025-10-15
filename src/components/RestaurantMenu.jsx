ce57780fa13ecc5b7b170ef88d27e7af5bfc693b
Dylan Pee <peedylan94@gmail.com>
2025-10-15 16:11:03 +1030

    Ep 07: List menu items for individual restaurant

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,26 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import mockRestaurantMenuData from "../utils/mockRestaurantMenu.json" assert { type: "json" };
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router";
 
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
+
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const url = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0035068&lng=77.5890953&restaurantId=425&catalog_qa=undefined&submitAction=ENTER";
-    const res = await fetch("https://corsproxy.io/?url=" + url);
-    console.log(res.ok, res.status);
-    const text = await res.text();
-    console.log(text)
+    setResInfo(mockRestaurantMenuData);
   };
+
+  if (resInfo === null) return <Shimmer count={6} />;
+
+  const { itemCards } = resInfo?.card?.card;
 
   return (
     <div className="menu">
       <h1>Restaurant Name</h1>
       <h1>Menu</h1>
       <ul>
-        <li>Burger</li>
-        <li>Fried Rice</li>
-        <li>Diet Coke</li>
+        {itemCards.map((item) => <li key={item?.card?.info?.id}>{item?.card?.info?.name} - Rs.{item?.card?.info?.price}</li>)}
       </ul>
     </div>
   );
