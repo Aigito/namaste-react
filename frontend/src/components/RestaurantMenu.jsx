c88098ed4d727ece64a6102673b7f18693aeafc4
Dylan Pee <peedylan94@gmail.com>
2025-10-17 16:58:10 +1030

    Ep 07: Moving frontend and backend components to respective directories

--- frontend/src/components/RestaurantMenu.jsx
+++ frontend/src/components/RestaurantMenu.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import mockRestaurantMenuData from "../utils/mockRestaurantMenu.json" assert { type: "json" };
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router";
 
@@ -12,19 +11,20 @@ const RestaurantMenu = () => {
   }, []);
 
   const fetchMenu = async () => {
-    setResInfo(mockRestaurantMenuData);
+    const response = await fetch("http://localhost:5000/api/restaurants");
+    const data = await response.json();
+
+    console.log(data);
   };
 
   if (resInfo === null) return <Shimmer count={6} />;
-
-  const { itemCards } = resInfo?.card?.card;
 
   return (
     <div className="menu">
       <h1>Restaurant Name</h1>
       <h1>Menu</h1>
       <ul>
-        {itemCards.map((item) => <li key={item?.card?.info?.id}>{item?.card?.info?.name} - Rs.{item?.card?.info?.price}</li>)}
+        {/* {itemCards.map((item) => <li key={item?.card?.info?.id}>{item?.card?.info?.name} - Rs.{item?.card?.info?.price}</li>)} */}
       </ul>
     </div>
   );
