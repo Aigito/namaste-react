d4829079f423620d19f51c3ec191edb5f6194e17
Dylan Pee <peedylan94@gmail.com>
2025-10-27 17:27:03 +1030

    Ep 08: Showcasing how to implement setState in functional and class based components

--- frontend/src/components/UserClass.jsx
+++ frontend/src/components/UserClass.jsx
@@ -3,12 +3,18 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0
+    };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
+        <h1>Count = {count}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: peedylan94@gmail.com</h4>
